38f286b8d8376b8e66c18a08f53512c82cca9a9b
prasath <prasath1196@gmail.com>
2019-04-18 23:08:22 +0530

    API versioning

---
+++ app/lib/api_version.rb
@@ -0,0 +1,16 @@
+class ApiVersion
+  attr_reader :version,:default
+  def initialize(version, default = false)
+    @version = version
+    @default = default
+  end
+
+  def matches?(request)
+    check_headers(request.headers) || default
+  end
+
+  def check_headers(headers)
+    accept =  headers[:accept]
+    accept && accept.include?("application/vnd.todos.#{version}+json")
+  end
+end
